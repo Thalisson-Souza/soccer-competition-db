a6c69ce9c01b8e5685031a59c036fbc31906ee95
Thalisson Souza <thalisson.souza@acad.ufsm.br>
2025-05-06 00:20:20 -0300

    feat: add updateTeam, findTeamById and deleteById

--- src/main/java/repository/TeamRepository.java
+++ src/main/java/repository/TeamRepository.java
@@ -1,5 +1,6 @@
 package main.java.repository;
 
+import main.java.entity.Coach;
 import main.java.entity.Team;
 
 import java.sql.*;
@@ -43,6 +44,76 @@ public class TeamRepository {
                 teams.add(team);
             }
             return teams;
+        }
+    }
+
+    public Team updateTeam(Team time) throws SQLException {
+        String sql = "UPDATE Team SET name = ?, stadium = ?, city = ?, foundation_date = ?, coach_id = ? WHERE id = ?";
+
+        try (PreparedStatement update = connect.prepareStatement(sql)) {
+            update.setString(1, time.getName());
+            update.setString(2, time.getEstadium());
+            update.setString(3, time.getCity());
+            update.setDate(4, java.sql.Date.valueOf(time.getFoundationDate()));
+            update.setLong(5, time.getCoach().getId());
+            update.setLong(6, time.getId());
+
+            int linhasAfetadas = update.executeUpdate();
+            if (linhasAfetadas > 0) {
+                System.out.println("Time atualizado com sucesso!");
+            }else {
+                System.out.println("Não encontrei time pelo ID dado");
+            }
+            return time;
+        }
+
+
+    }
+
+    public Team findTeamById(Long id) throws SQLException {
+        String sql = "SELECT t.id, t.name, t.stadium, t.city, t.foundation_date, c.id as coach_id, c.name as coach_name " +
+                "FROM Team t " +
+                "LEFT JOIN Coach c ON t.coach_id = c.id " +
+                "WHERE t.id = ?";
+
+
+        try (PreparedStatement find = connect.prepareStatement(sql)) {
+            find.setLong(1, id);
+
+            try (ResultSet rs = find.executeQuery()) {
+                if (rs.next()) {
+                    Team team = new Team();
+                    team.setId(rs.getLong("id"));
+                    team.setName(rs.getString("name"));
+                    team.setEstadium(rs.getString("stadium"));
+                    team.setCity(rs.getString("city"));
+                    team.setFoundationDate(rs.getDate("foundation_date").toLocalDate());
+
+                    Coach coach = new Coach();
+                    coach.setId(rs.getLong("coach_id"));
+                    coach.setName(rs.getString("name"));
+                    team.setCoach(coach);
+
+                    return team;
+                }else{
+                    return null;
+                }
+            }
+        }
+    }
+
+    public void deleteById(Long id) throws SQLException{
+        String sql = "DELETE FROM Team WHERE id = ?";
+
+        try (PreparedStatement delete = connect.prepareStatement(sql)) {
+            delete.setLong(1, id);
+
+            int rows = delete.executeUpdate();
+            if (rows > 0) {
+                System.out.println("Time deletado com sucesso!");
+            } else {
+                System.out.println("Não encontrado o ID dado");
+            }
         }
     }
 }
--- src/main/java/services/TeamService.java
+++ src/main/java/services/TeamService.java
@@ -5,6 +5,7 @@ import main.java.repository.TeamRepository;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 public class TeamService {
     private TeamRepository repository;
@@ -15,7 +16,7 @@ public class TeamService {
     }
 
     public void addTeam(Team team) throws SQLException {
-        if (team.getJogadores().size() > MAX_PLAYERS){
+        if (team.getJogadores().size() > MAX_PLAYERS) {
             throw new IllegalArgumentException("Jogadores cannot have more than 25 jogadores.");
         }
         repository.saveTeam(team);
@@ -23,5 +24,20 @@ public class TeamService {
 
     public List<Team> listTeam() throws SQLException {
         return repository.findAllTeams();
+    }
+
+    public void updateTeam(Team team) throws SQLException {
+        if (team.getJogadores().size() > MAX_PLAYERS) {
+            throw new IllegalArgumentException("Jogadores cannot have more than 25 jogadores.");
+        }
+        repository.updateTeam(team);
+    }
+
+    public void searchTeam (Long id) throws SQLException {
+        repository.findTeamById(id);
+    }
+
+    public void deleteTeam(Long id) throws SQLException {
+        repository.deleteById(id);
     }
 }
